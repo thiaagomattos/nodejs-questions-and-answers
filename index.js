98fb087597ed6cc1ca8daaa82982a788fb9418d2
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-14 11:03:00 -0300

    added params

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const app = express();
 //dizendo para o  express usar ejs como view engine
 app.set('view engine', 'ejs');
 
-app.get("/",(req, res) => {
-    res.render("index");
+app.get("/:name/:lang",(req, res) => {
+    var name = req.params.name;
+    var lang = req.params.lang;
+    res.render("index",{
+        name: name,
+        lang: lang,
+        enterprise: "thiago systems",
+        subscribers: 10000
+
+    });
 });
 
 app.listen(8080,()=>{console.log("app running!");});
