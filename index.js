8da63deafbb4ff6c9ba986dae051feeb7a1f12f0
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-14 11:21:40 -0300

    added more ejs examples

--- index.js
+++ index.js
@@ -7,11 +7,23 @@ app.set('view engine', 'ejs');
 app.get("/:name/:lang",(req, res) => {
     var name = req.params.name;
     var lang = req.params.lang;
+    var showMsg = false;
+
+    var products = [
+        {name: "doritos",price: 3.14},
+        {name: "coke",price: 5},
+        {name: "milk",price: 2.30}
+
+    ]
+
+
     res.render("index",{
         name: name,
         lang: lang,
         enterprise: "thiago systems",
-        subscribers: 10000
+        subscribers: 10000,
+        msg: showMsg,
+        products: products
 
     });
 });
